08194f7e1a49cecba9f28090e9665a1e54a32144
Tyler Green <greent@tyleragreen.com>
2018-11-27 07:09:35 -0500

    Add failing inheritance test

---
+++ tests/test_inheritance.py
@@ -0,0 +1,44 @@
+from interface import interface
+
+class Iterable:
+    def do_loop(self):
+        pass
+
+
+class AbstractList:
+    def another_method(self):
+        return 'not much here'
+
+
+class VehicleList(AbstractList):
+    def another_method(self):
+        return 'vehicles are cool'
+
+
+@interface(Iterable)
+class BusList(VehicleList):
+    def do_loop(self):
+        return 'looping bus'
+
+    def another_method(self):
+        return 'especially buses'
+
+
+def test_type_respects_inheritance():
+    bl = BusList()
+    assert type(bl) == BusList
+    assert type(bl) != Iterable
+    assert type(bl) != VehicleList
+
+
+def test_isinstance_respect_inheritance():
+    vl = VehicleList()
+    assert isinstance(vl, VehicleList)
+    assert isinstance(vl, AbstractList)
+
+    bl = BusList()
+    assert isinstance(bl, BusList)
+    # TODO these two should be true
+    assert isinstance(bl, VehicleList)
+    assert isinstance(bl, AbstractList)
+    assert not isinstance(bl, Iterable)
