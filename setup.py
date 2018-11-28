127989d42f33bcee1d65f91d262e2b60cef7592b
Tyler Green <greent@tyleragreen.com>
2018-11-28 07:25:55 -0500

    Add version file and setup

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+import io
+
 from setuptools import setup, find_packages
+
+about = dict()
+with io.open('interface/__version__.py', 'r', encoding='utf-8') as f:
+    exec(f.read(), about)
 
 setup(
     name="python-interface",
+    version=about['__version__'],
     packages=find_packages(include=['interface'])
 )
