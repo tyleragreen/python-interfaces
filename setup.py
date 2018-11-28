965a82d736968768adf58f6526a78f551254a93d
Tyler Green <greent@tyleragreen.com>
2018-11-28 07:31:41 -0500

    More setup fields

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ import io
 
 from setuptools import setup, find_packages
 
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 about = dict()
 with io.open('interface/__version__.py', 'r', encoding='utf-8') as f:
     exec(f.read(), about)
@@ -9,5 +13,10 @@ with io.open('interface/__version__.py', 'r', encoding='utf-8') as f:
 setup(
     name="python-interface",
     version=about['__version__'],
+    description='Bringing interfaces to Python.',
+    long_description=readme,
+    author='Tyler Green',
+    author_email='greent@tyleragreen.com',
+    url='https://github.com/tyleragreen/python-interface',
     packages=find_packages(include=['interface'])
 )
